273306bf24fe348e060dda07d6676f44ab99c774
dolly.ograce <dolly.ograce@gmail.com>
2022-04-06 10:29:19 -0400

    Updates' :exit

---
+++ src/test/java/com/jokesapi/JokesapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jokesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokesapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
